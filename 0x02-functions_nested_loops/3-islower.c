64498f88dd3cb0d5f96cfda753ec16bf71debcc7
fadilat25 <jyahaya50@gmail.com>
2022-12-13 13:25:40 -0800

    a function taht checks for lowercase xters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - checks if the character 'c' is a lowercase character
+ *
+ *Return: returns a value based on preset conditions
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
