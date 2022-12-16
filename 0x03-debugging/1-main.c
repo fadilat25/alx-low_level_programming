9772b50655490cea128f468d2a4a8f666530019a
fadilat25 <jyahaya50@gmail.com>
2022-12-16 12:43:03 -0800

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
 /**
  *while (i < 10)
@@ -16,6 +16,6 @@ i = 0;
  *putchar(i);
  *}
  */
-printf("Infinite loop avoided! \\o\/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
